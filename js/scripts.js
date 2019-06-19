72f9a4979a1f64da9188e7036c021543b4f2fc01
paityn <paityn626@hotmail.com>
2019-06-19 13:26:54 +0000

    uhhhh manipchal

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,23 @@
 // 1: Set the text of the <h1> element
-
+const heading = document.querySelector('h1');
+heading.textContent = "and i oop-";
 
 // 2: Set the color of the <h1> to a different color
-
+heading.style.color = "purple";
 
 // 3: Set the content of the '.desc' paragraph
 // The content should include at least one HTML tag
-
+const step3P = document.querySelector('.desc');
+step3P.innerHTML = "<b>i just hit my balls</b>";
 
 // 4: Set the class of the <ul> to 'list'
-
+const classList = document.querySelector("ul");
+classList.className = 'list';
 
 // 5: Create a new list item and add it to the <ul>
-
+const listItem = document.createElement("li");
+listItem.innerHTML = "<input> oh no"
+classList.appendChild(listItem);
 
 // 6: Change all <input> elements from text fields to checkboxes
 
@@ -20,6 +25,6 @@
 
 // 7: Create a <button> element, and set its text to 'Delete'
 // Add the <button> inside the '.extra' <div>
-
+const myButton = document.createElement
 
 // 8: Remove the '.extra' <div> element from the DOM when a user clicks the 'Delete' button
